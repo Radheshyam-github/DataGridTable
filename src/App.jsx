682fdc267855f0ce58b10f273ae5c66fa68d939c
Radheshyam <124288883+Radheshyam-github@users.noreply.github.com>
2024-07-22 08:51:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import DataGridComponent from './Components/DataGridComponent';
+
+function App() {
+  return (
+    <div className="App ">
+    <header className="App-header h-25">
+      <h1>Datagrid Using Material UI </h1>
+    </header>
+    <div className="container">
+      <DataGridComponent />
+    </div>
+  </div>
+  );
+}
+
+export default App;
